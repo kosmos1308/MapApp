9fdd0739a96662074ce78d5c24e823a183e5ff07
kosmos1308 <pasha13kosmos@gmail.com>
2022-08-30 22:52:07 +0300

    Add assets and create Models

---
+++ MapApp/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  MapApp
+//
+//  Created by pavel on 30.08.22.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
