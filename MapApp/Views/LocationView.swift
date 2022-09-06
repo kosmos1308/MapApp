8a19f981294f94ed4de2c684e3a457020c12430d
kosmos1308 <pasha13kosmos@gmail.com>
2022-09-06 21:13:37 +0300

    Add scaleEffect on LocationMapAnnotationView and tapGesture

--- MapApp/Views/LocationView.swift
+++ MapApp/Views/LocationView.swift
@@ -14,33 +14,14 @@ struct LocationView: View {
  
     var body: some View {
         ZStack {
-            Map(coordinateRegion: $vm.mapRegion,
-                annotationItems: vm.locations) { location in
-                MapAnnotation(coordinate: location.coordinates) {
-                    Text("Hello")
-                    LocationMapAnnotationView()
-                }
-            }
+            mapLayer
                 .ignoresSafeArea()
             
             VStack(spacing: 0) {
                 header
                     .padding()
-                
                 Spacer()
-                
-                ZStack {
-                    ForEach(vm.locations) { location in
-                        if vm.mapLocation == location {
-                            LocationPreviewView(location: location)
-                        }
-                    }
-                }
-                .shadow(color: .black.opacity(0.3), radius: 20)
-                .padding()
-                .transition(.asymmetric(
-                    insertion: .move(edge: .trailing),
-                    removal: .move(edge: .leading)))
+                locationPreviewStack
             }
         }
     }
@@ -85,5 +66,34 @@ extension LocationView {
                 radius: 20,
                 x: 0,
                 y: 15)
+    }
+    
+    private var mapLayer: some View {
+        Map(coordinateRegion: $vm.mapRegion,
+            annotationItems: vm.locations) { location in
+            MapAnnotation(coordinate: location.coordinates) {
+                LocationMapAnnotationView()
+                    .shadow(radius: 10)
+                    .scaleEffect(vm.mapLocation == location ? 1 : 0.7)
+                    .onTapGesture {
+                        vm.showNextLocation(location: location)
+                    }
+            }
+        }
+    }
+    
+    private var locationPreviewStack: some View {
+        ZStack {
+            ForEach(vm.locations) { location in
+                if vm.mapLocation == location {
+                    LocationPreviewView(location: location)
+                }
+            }
+        }
+        .shadow(color: .black.opacity(0.3), radius: 20)
+        .padding()
+        .transition(.asymmetric(
+            insertion: .move(edge: .trailing),
+            removal: .move(edge: .leading)))
     }
 }
