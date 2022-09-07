8fe0a9d8fba1d99b32104249f105ecd5060e5266
kosmos1308 <pasha13kosmos@gmail.com>
2022-09-07 23:48:03 +0300

    Create LocationDetatilView

---
+++ MapApp/Views/LocationDetailView.swift
@@ -0,0 +1,120 @@
+//
+//  LocationDetatilView.swift
+//  MapApp
+//
+//  Created by pavel on 7.09.22.
+//
+
+import SwiftUI
+import MapKit
+
+struct LocationDetailView: View {
+    
+    let location: Location
+    @EnvironmentObject private var vm: LocationViewModel
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                imageSection
+                    .shadow(color: .black.opacity(0.3),
+                            radius: 20,
+                            x: 0,
+                            y: 10)
+                
+                VStack(alignment: .leading, spacing: 16) {
+                    titleSection
+                    Divider()
+                    descriptionSection
+                    Divider()
+                    mapLayer
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+            }
+        }
+        .ignoresSafeArea()
+        .background(.ultraThinMaterial)
+        .overlay(alignment: .topLeading) {
+            backButton
+        }
+    }
+}
+
+struct LocationDetatilView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationDetailView(location: LocationsDataService.locations.first!)
+            .environmentObject(LocationViewModel())
+    }
+        
+}
+
+extension LocationDetailView {
+    private var imageSection: some View {
+        TabView {
+            ForEach(location.imageNames, id: \.self) {
+                Image($0)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: UIScreen.main.bounds.width)
+                    .clipped()
+            }
+        }
+        .frame(height: 500)
+        .tabViewStyle(.page)
+    }
+    
+    private var titleSection: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text(location.name)
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            Text(location.cityName)
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+        }
+    }
+    
+    private var descriptionSection: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text(location.description)
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+            
+            if let url = URL(string: location.link) {
+                Link("Read more on Wikipedia", destination: url)
+                    .font(.headline)
+                    .foregroundColor(.blue)
+            }
+        }
+    }
+    
+    private var mapLayer: some View {
+        Map(coordinateRegion: .constant(MKCoordinateRegion(
+            center: location.coordinates,
+            span: vm.mapSpan)),
+            annotationItems: [location]) { location in
+            MapAnnotation(coordinate: location.coordinates) {
+                LocationMapAnnotationView()
+                    .shadow(radius: 10)
+            }
+        }
+            .aspectRatio(1, contentMode: .fit)
+            .cornerRadius(30)
+    }
+    
+    private var backButton: some View {
+        Button {
+            
+        } label: {
+            Image(systemName: "xmark")
+                .font(.headline)
+                .foregroundColor(.primary)
+                .padding(16)
+                .background(.thickMaterial)
+                .cornerRadius(10)
+                .shadow(radius: 4)
+                .padding()
+        }
+    }
+}
